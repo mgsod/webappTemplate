9c63d4f6189ab9e4179e35cf34fb87b545ae9538
mashuai <mgso@foxmail.com>
2019-01-21 17:42:52 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <van-icon name="location-o"></van-icon>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style lang="less">
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    img {
+      border-radius: 50%;
+    }
+  }
+</style>
